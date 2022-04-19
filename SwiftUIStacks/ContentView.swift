72bb617f5008b95a8fe2e8cc1f0f9c3eb8e983c5
Mohsin Ali Ayub <meismosin@gmail.com>
2022-04-19 23:39:05 +0200

    Initial Commit

---
+++ SwiftUIStacks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIStacks
+//
+//  Created by Mohsin Ali Ayub on 19.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIStacks/SwiftUIStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStacksApp.swift
+//  SwiftUIStacks
+//
+//  Created by Mohsin Ali Ayub on 19.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
